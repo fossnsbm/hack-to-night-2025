a39a906f310b5bd0d6e22fb96c0322e9519d24aa
trevonakesh <trevonbusiness1@gmail.com>
2025-03-30 00:12:19 +0530

    feat/photo : created the images

--- platform/app/page.tsx
+++ platform/app/page.tsx
@@ -1,8 +1,11 @@
 import Header from "@/components/common/Header"
 import Footer from "@/components/common/Footer"
+import Memory from "@/components/Memory"
+
 
 export default function Home() {
     return (
+        <>
         <div id="page" className="snap-y snap-mandatory overflow-y-auto">
             <Header />
             <div id="hero" className="snap-start h-screen pt-[50px]">Hero</div>
@@ -11,7 +14,9 @@ export default function Home() {
             <div id="timeline" className="snap-start h-screen pt-[50px]">Timeline</div>
             <div id="register" className="snap-start h-screen pt-[50px]">Register</div>
             <div id="faq" className="snap-start h-screen pt-[50px]">Faq</div>
+            <Memory/>
             <Footer/>
         </div>
+        </>
     );
 }
