3db73ffe75783da317e5730442cb48b5c0a72b21
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-04-05 19:06:37 +0530

    style(faq): adds the shared styles

--- platform/components/FAQ/FAQSection.tsx
+++ platform/components/FAQ/FAQSection.tsx
@@ -2,13 +2,13 @@ import questions from "./Questions";
 
 export default function FAQSection() {
     return (
-            <div className="flex flex-col items-center justify-center text-center">
-                {questions.map(({ id, question, answer }) => (
-                    <div key={id} className="">
-                        <h2 className="text-lg font-bold mt-6">{question}</h2>
-                        <p className="text-gray-700 mb-6">{answer}</p>
-                    </div>
-                ))}
-            </div>
+        <div className="flex flex-col items-center justify-center text-center snap-start h-screen w-full py-[50px] px-4">
+            {questions.map(({ id, question, answer }) => (
+                <div key={id} className="">
+                    <h2 className="text-lg font-bold mt-6">{question}</h2>
+                    <p className="text-gray-700 mb-6">{answer}</p>
+                </div>
+            ))}
+        </div>
     );
 }
