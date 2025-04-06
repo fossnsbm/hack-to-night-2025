106222a45ae50ef13a79ab4ca04398b1bbe94b39
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-04-06 11:44:11 +0530

    ui(about): moves the section to the center

--- platform/components/About.tsx
+++ platform/components/About.tsx
@@ -4,14 +4,15 @@ import React, { useState, useEffect } from "react";
 import * as motion from "motion/react-client";
 
 function TextStream() {
-    const text = "amet, eiusmod ut ut sit aliquip Lorem nisi adipiscing do dolore laboris veniam, et ex exercitation ea consectetur Ut ullamco consequat. tempor magna incididunt Sed enim aliqua. elit. nostrud ipsum labore commodo ad quis minim dolor amet, laboris commodo ut eiusmod dolor Ut elit. quis veniam, ut Sed consectetur exercitation sit ullamco adipiscing dolore nisi consequat. tempor ea labore ad do minim et ipsum aliqua. aliquip Lorem enim incididunt ex nostrud magna veniam, Ut amet, laboris nostrud incididunt eiusmod ad";
+    const text =
+        "amet, eiusmod ut ut sit aliquip Lorem nisi adipiscing do dolore laboris veniam, et ex exercitation ea consectetur Ut ullamco consequat. tempor magna incididunt Sed enim aliqua. elit. nostrud ipsum labore commodo ad quis minim dolor amet, laboris commodo ut eiusmod dolor Ut elit. quis veniam, ut Sed consectetur exercitation sit ullamco adipiscing dolore nisi consequat. tempor ea labore ad do minim et ipsum aliqua. aliquip Lorem enim incididunt ex nostrud magna veniam, Ut amet, laboris nostrud incididunt eiusmod ad";
 
     const [displayedCharCount, setDisplayedCharCount] = useState(0);
 
     useEffect(() => {
         if (displayedCharCount >= text.length) return;
         const wordTimer = setTimeout(() => {
-            setDisplayedCharCount(prevCount => prevCount + 1);
+            setDisplayedCharCount((prevCount) => prevCount + 1);
         }, 50);
         return () => clearTimeout(wordTimer);
     }, [displayedCharCount, text.length]);
@@ -33,14 +34,21 @@ function TextStream() {
     );
 }
 
-
 function About() {
     return (
         <section id="about-us">
-            <div className="w-full h-full flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center snap-start h-screen w-full py-[50px] px-4">
                 <div className="md:h-full w-full flex flex-col items-center gap-6 mt-[40%] md:mt-12 md:justify-center">
-                    <motion.div initial={{ scale: 0.8 }} whileInView={{ scale: 1, transition: { duration: 0.4 } }}>
-                        <h2 className="text-2xl md:text-3xl text-center mb-4">About Us</h2>
+                    <motion.div
+                        initial={{ scale: 0.8 }}
+                        whileInView={{
+                            scale: 1,
+                            transition: { duration: 0.4 },
+                        }}
+                    >
+                        <h2 className="text-2xl md:text-3xl text-center mb-4">
+                            About Us
+                        </h2>
                     </motion.div>
                     <TextStream />
                 </div>
@@ -48,6 +56,5 @@ function About() {
         </section>
     );
 }
-
 
 export default About;
