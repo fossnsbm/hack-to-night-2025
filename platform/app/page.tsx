348907aa3a4035327c22f5f167394844e1ae35bc
Sakith B. <pvsakith@gmail.com>
2025-04-09 21:59:01 +0530

    Feat/photos (#10)
     refactor: cleanup
    
    ---------
    
    Co-authored-by: trevonakesh <trevonbusiness1@gmail.com>

--- platform/app/page.tsx
+++ platform/app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from "@/components/landing/Hero";
 import FAQ from "@/components/landing/FAQ";
 import About from "@/components/landing/About";
+import Memories from "@/components/landing/Memories"
 
 export default function Home() {
     return (
         <>
             <div id="hero" className="snap-start h-screen w-full py-[50px] px-4"><Hero /></div>
             <div id="about" className="snap-start h-screen w-full py-[50px] px-4"><About/></div>
-            <div id="photos" className="snap-start h-screen w-full py-[50px] px-4">Photos</div>
+            <div id="photos" className="snap-start h-screen w-full py-[50px] px-4"><Memories /></div>
             <div id="timeline" className="snap-start h-screen w-full py-[50px] px-4">Timeline</div>
             <div id="register" className="snap-start h-screen w-full py-[50px] px-4">Register</div>
             <div id="faq" className="snap-start h-screen w-full py-[50px] px-4"><FAQ /></div>
