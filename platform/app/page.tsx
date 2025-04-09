45174a420ec32a76da5466c6bf701a3a526b0201
Sakith B. <pvsakith@gmail.com>
2025-04-09 21:53:26 +0530

    refactor: cleanup

--- platform/app/page.tsx
+++ platform/app/page.tsx
@@ -1,22 +1,17 @@
-import Header from "@/components/common/Header"
-import Footer from "@/components/common/Footer"
-import Memory from "@/components/Memory"
-
+import Hero from "@/components/landing/Hero";
+import FAQ from "@/components/landing/FAQ";
+import About from "@/components/landing/About";
+import Memories from "@/components/landing/Memories"
 
 export default function Home() {
     return (
         <>
-        <div id="page" className="snap-y snap-mandatory overflow-y-auto">
-            <Header />
-            <div id="hero" className="snap-start h-screen pt-[50px]">Hero</div>
-            <div id="about" className="snap-start h-screen pt-[50px]">About</div>
-            <div id="photos" className="snap-start h-screen pt-[50px]">Photos</div>
-            <div id="timeline" className="snap-start h-screen pt-[50px]">Timeline</div>
-            <div id="register" className="snap-start h-screen pt-[50px]">Register</div>
-            <div id="faq" className="snap-start h-screen pt-[50px]">Faq</div>
-            <Memory/>
-            <Footer/>
-        </div>
+            <div id="hero" className="snap-start h-screen w-full py-[50px] px-4"><Hero /></div>
+            <div id="about" className="snap-start h-screen w-full py-[50px] px-4"><About/></div>
+            <div id="photos" className="snap-start h-screen w-full py-[50px] px-4"><Memories /></div>
+            <div id="timeline" className="snap-start h-screen w-full py-[50px] px-4">Timeline</div>
+            <div id="register" className="snap-start h-screen w-full py-[50px] px-4">Register</div>
+            <div id="faq" className="snap-start h-screen w-full py-[50px] px-4"><FAQ /></div>
         </>
     );
 }
